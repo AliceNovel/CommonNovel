dc4d8beaee784b83074ec58ec2ef3a6bb82d76f1
Lemon73-Computing <Lemonade.flash.1164@gmail.com>
2025-08-19 00:47:13 +0900

    feat(ast): created ast for common-novel processing

---
+++ tests/CommonNovel.Tests/AST.cs
@@ -0,0 +1,58 @@
+namespace CommonNovel.Tests;
+
+public class ASTUnitTest
+{
+    [Fact]
+    public void TestAST()
+    {
+        // Arrange
+        string[] inputTokens =
+        [
+            "- ",
+            "Alice",
+            "[",
+            "Hi, there.",
+            "]"
+        ];
+
+        // [<type>, <arg1>, ({arg2}, ...)]
+        string[][] expectedAST =
+        [
+            ["CharacterName", "Alice"],
+            ["Message", "Hi, there."]
+        ];
+
+        // Act
+
+        // Assert
+    }
+
+    // This is an example for deplication of same command
+    [Fact]
+    public void TestAST_Ex3()
+    {
+        // Arrange
+        string[] inputTokens =
+        [
+            "- ",
+            "Alice",
+            "[",
+            "Hi, there.",
+            "]",
+            "[",
+            "Welcome!",
+            "]"
+        ];
+
+        // [<type>, <arg1>, ({arg2}, ...)]
+        string[][] expectedAST =
+        [
+            ["CharacterName", "Alice"],
+            ["Message", "Welcome!"]
+        ];
+
+        // Act
+
+        // Assert
+    }
+}
