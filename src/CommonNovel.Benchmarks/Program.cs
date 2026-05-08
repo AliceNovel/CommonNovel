a75e37cf520946c495f888a90a52ca68473f2587
lemon73 <lemonade.flash.1164@gmail.com>
2026-05-09 02:31:27 +0900

    chore(benchmark): add the benchmark tests

---
+++ src/CommonNovel.Benchmarks/Program.cs
@@ -0,0 +1,50 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+namespace CommonNovel.Benchmarks;
+
+public class Program
+{
+    public static void Main(string[] args)
+        => BenchmarkRunner.Run<Benchmarks>();
+}
+
+[MemoryDiagnoser]
+public class Benchmarks
+{
+    [Benchmark]
+    public void RunNoder()
+    {
+        string input =
+        """
+        - Alice
+        [Hi, Bob!]
+
+        - Bob
+        [Alice!]
+
+        """;
+
+        _ = Compiler.Noder(input);
+    }
+
+    [Benchmark]
+    public void RunParser()
+    {
+        string inputNode =
+        """
+        - Alice
+        [Hi, there.]
+        """;
+
+        _ = Compiler.Parse(inputNode);
+    }
+
+    [Benchmark]
+    public void RunExecutor()
+    {
+        string[] inputAST = ["CharacterName", "Alice"];
+
+        _ = Compiler.Executor(inputAST);
+    }
+}
